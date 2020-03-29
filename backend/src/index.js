0a1e9926b9fe75b9d1b96a3474e34411ec241858
cilas santos <cilassantos@MacBook-Pro-de-cilas.local>
2020-03-28 21:44:28 -0300

    validaçao na rota de insert da ong

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const { errors } =  require('celebrate')
 const routes = require('./routes')
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 
-app.listen(3333,'0.0.0.0')
+app.listen(3333)
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = express.Router()
 
+const { celebrate, Segments, Joi } = require('celebrate')
+
 const ongController = require('./controllers/OngController')
 const incidentController = require('./controllers/IncidentController')
 const profileController = require('./controllers/ProfileController')
@@ -10,7 +12,16 @@ const sessionController = require('./controllers/SessionController')
 routes.post('/session', sessionController.create)
 
 routes.get('/ongs', ongController.list)
-routes.post('/ongs', ongController.insert)
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}),ongController.insert)
 
 routes.get('/profile', profileController.list)
 
